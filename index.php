76e67f6557932fcbc90dcbda57e201aab5619687
Arnol Cáceres <116393375+arnolcb@users.noreply.github.com>
2023-04-28 23:56:02 -0500

    v2 functional
    footer errors fixed

--- index.php
+++ index.php
@@ -1,79 +1,85 @@
 <?php
-  session_start();
+session_start();
 
-  if(isset($_SESSION['user'])){
-    header('Location: principal.php');
-  }
+if (isset($_SESSION['user'])) {
+  header('Location: principal.php');
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Login - paytableCalculator</title>
-    <link rel="stylesheet" href="assets/css/styles.css" />
-  </head>
-  <body>
 
-    <header>
-      <div>Método francés</div>
-      <nav>
-        <a href="#" class="button active">Home</a>
-        <a href="#" class="button">Us</a>
-        <a href="#" class="button">Contact Us</a>
-      </nav>
-    </header>
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Login - paytableCalculator</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="assets/css/styles.css" />
+</head>
 
-    <main>
-      <div class="contenedor_todo">
-        <div class="caja_trasera">
-          <div class="caja_trasera-login">
-            <h3>¿Ya tienes una cuenta?</h3>
-            <p>Inicia sesión para entrar en la página</p>
-            <button id="btn_iniciar-sesion">Iniciar Sesión</button>
-          </div>
+<body>
 
-          <div class="caja_trasera-register">
-            <h3>¿Aún no tienes una cuenta?</h3>
-            <p>Regístrate para entrar en la página</p>
-            <button id="btn_registrarse">Registrarse</button>
-          </div>
+  <header>
+    <div>Método francés</div>
+    <nav>
+      <a href="#" class="button active">Home</a>
+      <a href="#" class="button">Us</a>
+      <a href="#" class="button">Contact Us</a>
+    </nav>
+  </header>
+
+  <main>
+    <div class="contenedor_todo">
+      <div class="caja_trasera">
+        <div class="caja_trasera-login">
+          <h3>¿Ya tienes una cuenta?</h3>
+          <p>Inicia sesión para entrar en la página</p>
+          <button id="btn_iniciar-sesion">Iniciar Sesión</button>
         </div>
 
-        <div class="contenedor_login-register">
-          <!--Formulario LOGIN-->
-          <form action="php/login_usuario_be.php" method="POST" class="formulario_login">
-            <h2>Welcome, Let's Get Started</h2>
-            <input type="text" placeholder="Correo electrónico" name="email"/>
-            <input type="password" placeholder="Contraseña" name="password"/>
-            <button>Entrar</button>
-          </form>
-
-          <!--Formulario REGISTER-->
-          <form action="php/registro_usuario_be.php" method="POST" class="formulario_register">
-            <h2>Registrarse</h2>
-            <input type="text" placeholder="Nombre Completo" name="fullname"/>
-            <input type="text" placeholder="Usuario" name="user"/>
-            <input type="text" placeholder="Correo electrónico" name="mail" />
-            <input type="password" placeholder="Contraseña" name="password"/>
-            <button>Registrarse</button>
-          </form>
+        <div class="caja_trasera-register">
+          <h3>¿Aún no tienes una cuenta?</h3>
+          <p>Regístrate para entrar en la página</p>
+          <button id="btn_registrarse">Registrarse</button>
         </div>
       </div>
-    </main>
 
-    <footer>
-      <div>Método francés</div>
-      <div>
-        <a href="#" class="social"><img src="twitter.png"></a>
-        <a href="#" class="social"><img src="facebook.png"></a>
-        <a href="#" class="social"><img src="instagram.png"></a>
-        <a href="#" class="social"><img src="linkedin.png"></a>
+      <div class="contenedor_login-register">
+        <!--Formulario LOGIN-->
+        <form action="php/login_usuario_be.php" method="POST" class="formulario_login">
+          <h2>Welcome, Let's Get Started</h2>
+          <input type="text" placeholder="Correo electrónico" name="email" />
+          <input type="password" placeholder="Contraseña" name="password" />
+          <button>Entrar</button>
+        </form>
+
+        <!--Formulario REGISTER-->
+        <form action="php/registro_usuario_be.php" method="POST" class="formulario_register">
+          <h2>Registrarse</h2>
+          <input type="text" placeholder="Nombre Completo" name="fullname" />
+          <input type="text" placeholder="Usuario" name="user" />
+          <input type="text" placeholder="Correo electrónico" name="mail" />
+          <input type="password" placeholder="Contraseña" name="password" />
+          <button>Registrarse</button>
+        </form>
       </div>
-      <div class="rights">&copy; SV81 - Grupo 2, 2023.</div>
-    </footer>
+    </div>
+  </main>
 
-    <script src="assets/js/script.js"></script>
-  </body>
+  <footer>
+    <div>Método francés</div>
+    <div>
+      <a href="#" class="social"><span class="circle"><img src="assets/img/twitter_icon.png"></span></a>
+      <a href="#" class="social"><span class="circle"><img src="assets/img/facebook_icon.png"></span></a>
+      <a href="#" class="social"><span class="circle"><img src="assets/img/instagram_icon.png"></span></a>
+      <a href="#" class="social"><span class="circle"><img src="assets/img/linkedin_icon.png"></span></a>
+    </div>
+    <div class="rights">&copy; SV81 - Grupo 2, 2023.</div>
+  </footer>
+
+  <script src="assets/js/script.js"></script>
+</body>
+
 </html>
